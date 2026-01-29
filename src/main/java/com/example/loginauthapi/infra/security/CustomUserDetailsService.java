375d0a274fa58ce58ebf553a99749a09153f8348
Laynara S Vieira <contatolaynarasvieira@gmail.com>
2026-01-29 13:11:59 -0300

    finalizando api de register

--- src/main/java/com/example/loginauthapi/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import com.example.loginauthapi.domain.user.User;
 import com.example.loginauthapi.repositories.UserRepository;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
  @Autowired
  private UserRepository repository;
